5e3c7990116a9953a0b86814d0e3ef819ce95aef
Anshul <anshuldubey2166@gmail.com>
2017-03-28 14:57:17 -0400

    Only JS remains

--- views/store.php
+++ views/store.php
@@ -31,63 +31,51 @@ Now to have various category
 First Category-Top Sales like Kindle,PS4,Music Player,Jackets
 Make all horizontal
 -->
-<div id="category">
+<div class="products">
+                <h1>Best of Fashion</h1>
+				<div class="cl">&nbsp;</div>
+				<ul>
+				    <li>
+				    	<a href="#"><img src="../public/img/big1.jpg" alt="" /></a>
+				    	<div class="product-info">
+				    		<h3>Manchester</h3>
+				    		<div class="product-desc">
+								<h4>WOMEN’S</h4>
+				    			<p>Woolen Jacket<br /></p>
+				    			<strong class="price">6000</strong>
+				    		</div>
+				    	</div>
+			    	</li>
+			    	<li>
+				    	<a href="#"><img src="../public/img/big1.jpg" alt="" /></a>
+				    	<div class="product-info">
+				    		<h3>Being Human</h3>
+				    		<div class="product-desc">
+								<h4>MEN’S</h4>
+				    			<p>Men's Jacket and class<br /></p>
+				    			<strong class="price">6000</strong>
+				    		</div>
+				    	</div>
+			    	</li>
+			    	<li class="last">
+				    	<a href="#"><img src="../public/img/big1.jpg" alt="" /></a>
+				    	<div class="product-info">
+				    		<h3>Levis</h3>
+				    		<div class="product-desc">
+								<h4>Casual</h4>
+				    			<p>Comfort of class<br /></p>
+				    			<strong class="price">6000</strong>
+				    		</div>
+				    	</div>
+			    	</li>
+				</ul>
+				<div class="cl">&nbsp;</div>
+			</div>
+			<!-- End Products -->
+			<!-- Add other contents here like books,games....-->
+		</div>
+		<!-- End Content -->
 
-<h3>Top Purchases</h3>
-<img src="" alt="Jacket" onhover="Raymond Jacket">
-<img src="" alt="Kindle" onhover="Best Reader for book lovers">
-<img src="" alt="PS4" onhover="PS4">
-<img src="" alt="Music Player" onhover="Music Player">
-
-<!--
-Exclusive Discount Offers
-Make All horizontal
--->
-<h3>The Best Discounts Available</h3>
-<img src="" alt="Ear-Phones" onhover="Ear-Phones">
-<img src="" alt="Shoes" onhover="Adidas">
-<img src="" alt="Fifa 17" onhover="Fifa 17">
-<img src="" alt="Moto G Plus" onhover="Moto G Plus">
-
-<!--
-Now do catgeory wise exclusive product shows
--->
-<a href="Buy" name="electronics">
-<h3>Best Of Electronics</h3>
-<img src="" alt="HeadPhones" onhover="Sony Headphones">
-<img src="" alt="Lenovo Phone" onhover="Lenovo Phone">
-<img src="" alt="Binoculars" onhover="">
-<img src="" alt="Power Banks" onhover="5000mAh Tukzer">
-</a>
-
-
-<h3>Best Of Fashion</h3>
-<img src="" alt="Jeans" onhover="Jeans">
-<img src="" alt="Jacket" onhover="Jacket">
-<img src="" alt="Shirt" onhover="Shirt">
-<img src="" alt="Shoes" onhover="Shoes">
-
-
-<h3>Best Of Books</h3>
-<img src="" alt="The Secret Crusade" onhover="The Secret Crusade">
-<img src="" alt="The Black Flag" onhover="The Black Flag">
-<img src="" alt="The Adventures of Sherlock Holmes" onhover="The Adventures of Sherlock Holmes">
-<img src="" alt="Java Puzzlers" onhover="Java Puzzlers">
-
-
-<h3>Video Games</h3>
-<img src="" alt="Fifa 17" onhover="Fifa 17">
-<img src="" alt="DBC 17" onhover="DBC 17">
-<img src="" alt="Age of empires 2" onhover="Age of empires 2">
-<img src="" alt="Assassin's creed" onhover="Assassin's creed">
-
-
-<h3>PC and Other Accessories</h3>
-<img src="" alt="HP Laptop" onhover="HP Laptop">
-<img src="" alt="Mousepad" onhover="Mousepad">
-<img src="" alt="Xbox Controller" onhover="Xbox Controller">
-<img src="" alt="Keyboard" onhover="Keyboard">
-</div>
 </div>
 <footer>
 	<div class="wrapper">
