9bccafab1ba5f88420abe3290ef01d8df3086749
Anshul <anshuldubey2166@gmail.com>
2017-03-28 11:04:11 -0400

    Add Storefront

--- controllers/session.php
+++ controllers/session.php
@@ -3,7 +3,7 @@
 	// enable sessions
     session_start();
     // require authentication for all pages except /login.php, /logout.php, and /register.php
-    if (!in_array($_SERVER["PHP_SELF"], ["/Project_2/public/login.php", "/Project_2/public/logout.php", "/Project_2/public/register.php"]))
+    if (!in_array($_SERVER["PHP_SELF"], ["/Project_2/public/store.php", "/Project_2/public/login.php", "/Project_2/public/logout.php", "/Project_2/public/register.php"]))
     {
         if (empty($_SESSION["id"]))
         {
--- public/store.php
+++ public/store.php
@@ -1,3 +1,9 @@
 <?php
     
+    // configuration
+    require("../controllers/helpers.php"); 
+
+    // $arr = fetch_from_store();
+    // render("store.php", $arr);
+    render("store.php");
 ?>
