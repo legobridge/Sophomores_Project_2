1a64494f954046c591e32c4ce45c689fd88c8dae
Anshul <anshuldubey2166@gmail.com>
2017-03-28 12:27:42 -0400

    More CSS done

--- views/cp_form.php
+++ views/cp_form.php
@@ -1,15 +1,31 @@
-<form id="change_pwd" action="../public/changepwd.php" method="post">
-    <fieldset>
-        <div>
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Change your Password</title>
+  <link rel="stylesheet" href="../public/css/style.css">
+    
+</head>
+
+<body>
+  <div class="form">
+          <center><h2>Password Change</h2></center>
+    <form id="change_pwd" action="../public/changepwd.php" method="post">
+          <div class="field-wrap">
+            <label>
+              <span class="req" name="pass">*</span>
+            </label>
             <input name="pass" placeholder="Password" type="password"/>
-        </div>
-         <div>
+          </div>
+          
+          <div class="field-wrap">
+            <label>
+              <span class="req" name="cpass">*</span>
+            </label>
             <input name="confirmation" placeholder="Confirm Password" type="password"/>
-        </div>
-        <div>
-            <button type="submit">
-               Change Password 
-            </button>
-        </div>
-    </fieldset>
-</form>
+          </div>   
+          <button type="submit" class="button button-block"/>Change Password</button>
+      </form>
+    </div> 
+ </body>
+ </html>    
--- views/sell_form.php
+++ views/sell_form.php
@@ -1,6 +1,22 @@
-<form id="sell_form" action="../public/sell.php" method="post" enctype="multipart/form-data">
-    <fieldset>
-        <div>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Sign-Up/Login Form</title>
+  <link rel="stylesheet" href="../public/css/style.css">    
+</head>
+<body>
+ <div class="form">
+       <ul class="tab-group">
+        <li class="tab active"><a href="#signup">Sign Up</a></li>
+      </ul>
+      <div class="tab-content">
+        <div id="sign" style="display:inline">   
+          <h1>Sign Up for Free</h1>
+          
+          <form action="sell.php" method="post" name="myForm" onsubmit="return validateForm();">
+          
+           <div>
             <select name="category">
             <option selected disabled hidden>Select Category</option>
             <option value="books">Books</option>
@@ -10,34 +26,45 @@
             <option value="sports">Sports</option>
             <option value="vehicles">Vehicles</option>
             <option value="others">Others</option>
-        </select>
-        </div>
-        <div>
-            <input name="name" placeholder="Item Title (Min. length 4 characters)" type="text"/>
-        </div>
-        <div>
-            <input name="description" placeholder="Item Description (Max. length 200 characters)" type="text"/>
-        </div>
-        <div>
-            <input name="contact" placeholder="Contact Info" type="text"/>
-        </div>
-        <div>
-            <input type="radio" name="donate" value="yes" checked> I want to donate<br>
-            <input type="radio" name="donate" value="no"> I want to sell<br>
-        </div>
-        <div>
+           </select>
+           </div>
+          
+           <div class="field-wrap">
+    
+              <input autofocus name="name" placeholder="Item Title (Min. length 4 characters)" type="text"/>
+            </div>
+          
+
+          <div class="field-wrap">
+           
+           <input name="contact" placeholder="Contact Info" type="text"/>
+          </div>
+          
+          <div class="field-wrap">
+            
             <input name="price" placeholder="Your Price (in Rs.)" type="text"/>
-        </div>
-        <div>
-            <input type="hidden" name="MAX_FILE_SIZE" value="300000"/>
-        </div>
-        <div>Upload Image: 
-            <input name="picture" placeholder="Your Image" type="file"/>
-        </div>
-        <div>
-            <button type="submit">
-               Post Advertisement
-            </button>
-        </div>
-    </fieldset>
-</form>
+          </div>
+          
+          <div class="field-wrap">
+           <textarea name="description" form="myform">Item Description (Max. length 200 characters)</textarea>
+          </div>
+          
+          <div class="gender">
+           <input type="radio" name="donate" value="yes" checked> I want to donate<br>
+            <input type="radio" name="donate" value="no"> I want to sell<br>
+          </div>
+          <div class="field-wrap">
+          <input name="picture" placeholder="Your Image" type="file"/>          
+          </div>
+          <button type="submit" class="button button-block"/>Post Advertisement</button>
+          
+          </form>
+
+        
+     
+      </div><!-- tab-content -->
+</div> <!-- /form -->
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src="../public/js/index.js"></script>
+</body>
+</html>
